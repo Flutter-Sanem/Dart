090560bee6abee17eecdbda51baa8eb4edc81d10
bedirhan-1 <bedirhan9828@gmail.com>
2026-06-28 18:45:25 +0300

    first

---
+++ Dart/ders1-1.dart
@@ -0,0 +1,47 @@
+void main() {
+  int a = 5;
+  int b = 6;
+
+  // print(a > b || a < b);
+  // print(a > b && a < b);
+
+  // Yorum satırı yapmak için: ctrl + (k + c)
+  // Yorum satırı kapatmak için: ctrl + (k + u)
+
+  // IF-ELSE
+
+  // String sanemsPassword = "parola";
+  // String enteredPassword = "parolı";
+
+  // if (enteredPassword == sanemsPassword) {
+  //   print("Şifre doğru, giriş başarılı");
+  // } else {
+  //   print("Şifre yanlış, giriş başarısız");
+  // }
+
+  // IF ELSE ORNEK-1
+  // Program: 0-8 yaş arası bebek, 8-17 yaş arası çocuk, 18-25 yaş arası genç,
+  // 25-65 arası yetişkin, 65 ve üzeri yaşlı olacak şekilde kıyaslanan yaşın
+  // çıktısını versin
+
+  // int age = 18;
+
+  // if (age <= 8) {
+  //   print("Sen bebeksin");
+  // } else if (age > 8 && age <= 17) {
+  //   print("Sen çocuksun");
+  // } else if (age > 17 && age <= 25) {
+  //   print("Sen gençsin");
+  // } else if (age > 25 && age <= 65) {
+  //   print("Sen yetişkinsin");
+  // } else if (age > 65) {
+  //   print("Sen yaşlısın");
+  // }
+
+  // IF-ELSE TERNARY OPERATOR
+  // int age = 19;
+
+  // String girisYapabilirMi = age > 18 ? "Girebilir" : "Giremez";
+
+  // print(girisYapabilirMi);
+}
---
+++ Dart/ders1-2.dart
@@ -0,0 +1,15 @@
+// LİSTELER
+
+void main() {
+  // List myList = ["Sanem", "iş", "güç", "hayat", 14, 15, true, 3.6];
+
+  // List<int> myIntList = [1, 2, 3, 4, 5];
+
+  // List<double> myDoubleList = [1.5, 2.8, 6.1, 4.4];
+
+  // List<String> meyveler = ['Elma', 'Armut'];
+
+  List<String> myStringList = ["anne", "baba", "teyze", "hala"];
+
+  print(myStringList[2][3]); // "z"
+}
